e015662bc178c35827b781a364734e90905466eb
Avate <Avate.Avil@bcg.com>
2017-09-28 12:08:32 +0530

    VS Code mmsql plugin sample.

---
+++ samplesql.sql
@@ -0,0 +1,14 @@
+-- CREATE TABLE Persons (
+--     PersonID int,
+--     LastName varchar(255),
+--     FirstName varchar(255),
+--     Address varchar(255),
+--     City varchar(255) 
+-- );
+use sampledb;
+
+
+insert into Persons (PersonID, LastName, FirstName,
+Address, City) VALUES(0,'A','A','Bang','KA');
+
+select * from Persons;
